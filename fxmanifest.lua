c668d58c5e96606eca1cb9824fe767c358ee25da
admincentralsp <admin.centralsp@gmail.com>
2026-09-03 00:49:52 -0300

    Add files via upload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,29 +1,19 @@
-fx_version 'adamant'
-game 'gta5'
-lua54 'yes'
-author 'https://github.com/Krisuniooo'
-description 'Radar Gun applied to 3D model with DUI'
-version '1.0.0'
-shared_scripts {
-    '@vrp/lib/utils.lua'
+lua54 'on'
+fx_version 'cerulean'
+games {'gta5'}
+description 'Add-on weapon generated using FiveM Addon Weapon Tool Kit'
+files{
+	'**/weaponcomponents.meta',
+	'**/weaponarchetypes.meta',
+	'**/weaponanimations.meta',
+	'**/pedpersonality.meta',
+	'**/weapons.meta',
 }
-client_scripts {
-	'config.lua',
-	'client/client.lua',
-}
-server_scripts {
-    'server/server.lua',
-}
-ui_page 'ui/index.html'
-files {
-    'ui/index.html',
-    'ui/style.css',
-    'ui/script.js',
-    'metas/*.meta',
-}
-data_file 'WEAPON_METADATA_FILE' 'metas/weaponarchetypes.meta'
-data_file 'WEAPON_ANIMATIONS_FILE' 'metas/weaponanimations.meta'
-data_file 'CONTENT_UNLOCKING_META_FILE' 'metas/contentunlocks.meta'
-data_file 'PED_PERSONALITY_FILE' 'metas/pedpersonality.meta'
-data_file 'WEAPONINFO_FILE' 'metas/weapons.meta'
+data_file 'WEAPONCOMPONENTSINFO_FILE' '**/weaponcomponents.meta'
+data_file 'WEAPON_METADATA_FILE' '**/weaponarchetypes.meta'
+data_file 'WEAPON_ANIMATIONS_FILE' '**/weaponanimations.meta'
+data_file 'PED_PERSONALITY_FILE' '**/pedpersonality.meta'
+data_file 'WEAPONINFO_FILE' '**/weapons.meta'
+client_script 'cl_weaponNames.lua'
+dependency '/assetpacks'
 
